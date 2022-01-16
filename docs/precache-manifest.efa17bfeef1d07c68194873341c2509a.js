a94e2ddf9a55346877e2b8ebba9c34bd3e05faee
Javier Lucero <javimcl1986@gmail.com>
2022-01-15 19:22:45 -0500

    agrega docs

---
+++ docs/precache-manifest.efa17bfeef1d07c68194873341c2509a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ab5345f120bce37a14682f241047e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8db163ce42a9229f66f4",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db163ce42a9229f66f4",
+    "url": "/static/js/main.b8aa5c18.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
